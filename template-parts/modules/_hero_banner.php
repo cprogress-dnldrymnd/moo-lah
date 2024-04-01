0cff13fccac4106e95f0c45e25fbb106507c9308
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-01 12:32:46 +0800

    css

--- template-parts/modules/_hero_banner.php
+++ template-parts/modules/_hero_banner.php
@@ -14,7 +14,6 @@ $id = $module['id'] != '' ?  $module['id'] : $section_id;
 <section id="<?= $id ?>" class="hero-banner <?php $DisplayData->get_classes($section_classes) ?>">
 	<div class="circle" data-aos="fade-up-right" data-aos-duration="1000"></div>
 	<div class="circle-3" data-aos="fade-right" data-aos-duration="1000"></div>
-	<div class="circle-2" data-aos="fade-up" data-aos-duration="1000"></div>
 
 	<div class="<?= $container ?>">
 		<div class="inner content-holder <?= $text_align ?> color-white">
